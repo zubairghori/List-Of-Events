04b074336d29edef550fab4870cb51b2114fe58f
Admin <zubairghori93@gmail.com>
2018-03-23 02:11:39 +0500

    Controllers created
    1. Events tableview controllers
    2. Events Images viewcontroller

---
+++ ListOfEvents/ListOfEvents/Controllers/EventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsTableViewCell.swift
+//  ListOfEvents
+//
+//  Created by Admin on 23/03/2018.
+//  Copyright © 2018 Zubair Ghori. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ListOfEvents/ListOfEvents/Controllers/ImagesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImagesTableViewCell.swift
+//  ListOfEvents
+//
+//  Created by Admin on 23/03/2018.
+//  Copyright © 2018 Zubair Ghori. All rights reserved.
+//
+
+import UIKit
+
+class ImagesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
